1c064a0e22e184291f4df43d45d784ed09a3160e
hoan9coder <117622199+hoan9coder@users.noreply.github.com>
2023-06-17 18:14:45 +0700

    Add files via upload

---
+++ js/muahang.js
@@ -0,0 +1,24 @@
+const buttons = document.querySelectorAll(".button");
+
+buttons.forEach(function(button) {
+  button.addEventListener("click", function(event) {
+    var product = event.target.closest(".content-div3-img").querySelector(".content-div3-text");
+    var productImg = event.target.closest(".content-div3-img").querySelector("img").src;
+    var productName = product.querySelector(".name").textContent;
+    var productPrice = product.querySelector(".cost").textContent;
+    addToCart(productName, productPrice, productImg);
+  });
+});
+
+function addToCart(name, price, imgUrl) {
+  var table = document.querySelector("table tbody");
+  var row = table.insertRow();
+  var nameCell = row.insertCell(0);
+  var priceCell = row.insertCell(1);
+  var imgCell = row.insertCell(0);
+  nameCell.innerHTML = name;
+  priceCell.innerHTML = price;
+  imgCell.innerHTML = "<img src='" + imgUrl + "' width='50' />";
+}
+
+
